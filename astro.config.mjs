e26920320e0e8aaadea2b85e297c9800b4f5b53b
Marius Toft <marius.toft@icloud.com>
2024-01-27 13:14:07 +0000

    Implement ui elements

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,5 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), htmx()],
   output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  adapter: vercel(),
 });
